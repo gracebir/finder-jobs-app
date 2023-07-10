3a4ade0216bd03294372735c46de86125c732b4d
gracebir <grace.devolop@gmail.com>
2023-07-10 20:27:58 +0200

    fix the url link

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,7 @@
 export const checkImageUrl = (url) => {
     if(!url) return false
     else {
-        const pattern = new RegExp('^https?\\/\\/.+\\.(png|jpg|jpeg|bmg|gif|webp)$', 'i')
+        const pattern = new RegExp('^https?:\\/\\/.+\\.(png|jpg|jpeg|bmg|gif|webp)$', 'i')
         return pattern.test(url)
     }
 }
